fe597e5500902d1410d024a5f7422d2302c7e2a9
Alejandro <aam0063@alu.medac.es>
2026-05-14 10:58:08 +0200

    a

--- Oracle SQL/bdor2.sql
+++ Oracle SQL/bdor2.sql
@@ -1,3 +1,9 @@
+drop type moto cascade;
+drop type CocheElectrico cascade;
+drop type Vehiculo cascade;    
+    
+create or replace type Vehiculo as object (
+    matricula varchar2(10),
     marca varchar2(50),
     modelo varchar2(50),
     anio number(4),
@@ -10,3 +16,22 @@ create or replace type body Vehiculo as
         return extract(YEAR from sysdate) - anio;
     end;
 end;
+
+create or replace type CocheElectrico under Vehiculo(
+    autonnomia number,
+    member procedure aumentarAutonomia(incremento number)
+);
+
+create or replace type body CocheElectrico as
+    member procedure aumentarAutonomia(incremento number) is
+    begin
+        self.autonomia := self.autonomia + incremento;
+    end;
+    
+create or replace type moto under vehiculo(
+    cilindrada number;
+);
+
+create table CocheElectricos of CocheElectrico;
+create table Motos of Moto;
+
